2b2f85195e3a6bc72550f05ed06014e1cb281963
Yury Glushkov <mail@glushkov.us>
2019-03-18 14:53:49 +0300

    Log provider registration converted to hangfire style

---
+++ src/Lykke.Logs.Hangfire/GlobalConfigurationExtension.cs
@@ -0,0 +1,26 @@
+using System;
+using Autofac;
+using Hangfire;
+using JetBrains.Annotations;
+using Lykke.Common.Log;
+
+namespace Lykke.Logs.Hangfire
+{
+    public static class GlobalConfigurationExtension
+    {
+        public static IGlobalConfiguration<LykkeLogProvider> UseLykkeLogProvider(
+            [NotNull] this IGlobalConfiguration configuration,
+            [NotNull] IContainer container)
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof (configuration));
+            }
+
+            var logFactory = container.Resolve<ILogFactory>();
+            var logProvider = new LykkeLogProvider(logFactory);
+            
+            return configuration.UseLogProvider(logProvider);
+        }
+    }
+}
